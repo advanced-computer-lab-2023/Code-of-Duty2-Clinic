99cc70ace002ca5d2a549a7ea9ae23ee30ba9aef
AbdelrahmanSalehRewaished <abdelrahman.saleh@student.guc.edu.eg>
2023-10-06 21:20:38 +0300

    Added cors options as a middleware and edited the config properties

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import connectToDB from './config/database';
 import config from './config/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors(config.server.corsOptions));
+
 connectToDB()
 app.get('/', (req, res) => {
     res.send('Server Online!');
