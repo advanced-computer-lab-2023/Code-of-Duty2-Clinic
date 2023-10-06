ea41bc4d09141b2922e48b910d70e326ad9e7893
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2023-10-06 18:47:16 +0300

    Dynamically render registered family members with test data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,38 @@
 import './App.css'
+import PatientRegisteredFamilyMembers from './components/PatientRegisteredFamilyMembers'
 export default function App() {
-
+  let Patient = {
+    name: 'John Doe',
+    familyMembers: [
+      {
+        name: 'Jane Doe',
+        relationship: 'Mother'
+      },
+      {
+        name: 'Jack Doe',
+        relationship: 'Father'
+      },
+      {
+        name: 'Jill Doe',
+        relationship: 'Sister'
+      },
+      {
+        name: 'Jake Doe',
+        relationship: 'Brother'
+      },
+      {
+        name: 'Jenny Doe',
+        relationship: 'Sister'
+      },
+      {
+        name: 'Jade Doe',
+        relationship: 'Sister'
+      }
+    ]
+  };
   return (
     <>
-    <h1>React App Running</h1>
+    <PatientRegisteredFamilyMembers patient = {Patient}/>
     </>
   )
 }
