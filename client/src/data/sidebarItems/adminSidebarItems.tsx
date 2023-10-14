edccdcf88515d691252a0a8f9959f982487155cb
abdelrahmanAbouelkheir <abouelkheirabdelrahman@gmail.com>
2023-10-14 16:24:43 +0300

    Setting routes for Prescriptions and HealthPackages

--- client/src/data/sidebarItems/adminSidebarItems.tsx
+++ client/src/data/sidebarItems/adminSidebarItems.tsx
@@ -1,3 +1,15 @@
-export const adminSidebarItems = [
+import { HealthPackagesRoute } from "../routes/adminRoutes";
+import PersonIcon from '@mui/icons-material/Person';
 
+export const adminSidebarItems = [
+   { title: 'General',
+        items: [
+            {
+                title: 'Health Packages ',
+                href: HealthPackagesRoute.path,
+                icon: <PersonIcon />
+            },
+            
+        ]
+   }
 ];
