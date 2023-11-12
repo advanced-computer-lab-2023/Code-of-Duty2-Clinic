6d72ba8e6511ed39d6991f7e249b8399b7d85310
AbdelrahmanSalehRewaished <abdelrahman.saleh@student.guc.edu.eg>
2023-11-12 16:47:06 +0200

    pending vewing details

--- client/src/pages/patients/ViewDoctorDetails.tsx
+++ client/src/pages/patients/ViewDoctorDetails.tsx
@@ -1,70 +1,59 @@
 import axios from "axios";
 import { DoctorDetails } from "../../types";
-import {useEffect, useState} from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { config } from "../../configuration";
 import { getFormattedDateTime } from "../../utils/formatter";
 
+const ViewDoctorDetails: React.FC = () => {
+  const [doctor, setDoctor] = useState<DoctorDetails>({} as DoctorDetails);
+  const { doctorId } = useParams();
+  const fetchDoctor = async () => {
+    try {
+      const response = await axios.get(
+        `${config.serverUri}/patients/doctors/${doctorId}`
+      );
+      console.log(response);
+      setDoctor(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const ViewDoctorDetails:React.FC = () => {
-    const [doctor, setDoctor] = useState<DoctorDetails>({} as DoctorDetails);
-    const doctorId  = useLocation().pathname.split('/')[3];
-    const fetchDoctor = async () => {
-        try {
-          const response = await axios.get(`${config.serverUri}/patients/doctors/${doctorId}`);
-          console.log(response)
-          setDoctor(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-    };
+  useEffect(() => {
+    fetchDoctor();
+  }, []);
 
-    useEffect(() => {
-        fetchDoctor();
-    }, []);
+  if (!doctor) return <p>Loading... </p>;
 
-    if(!doctor) return (<p>Loading... </p>)
-
-    return (
-        
-        <div>
-            <h1>View Doctor Details</h1>
-            <ul>
-                <li>
-                    Name: {doctor?.name}
-                </li>
-                <li>
-                    Speciality: {doctor?.speciality}
-                </li>
-                <li>
-                    Session price: {doctor?.sessionPrice}
-                </li>
-                <li>
-                    Email: {doctor?.email}
-                </li>
-                <li>
-                    Mobile Number: {doctor?.mobileNumber}
-                </li>
-                <li>
-                    Affiliation: {doctor?.affiliation}
-                </li>
-               <li>
-                    Educational Background: {doctor?.educationalBackground}
-               </li>
-               <li>
-                    Available time slots:
-                    <ul>
-                        {
-                            doctor?.availableSlots&&
-                            doctor?.availableSlots.map((slot:{startTime:string,endTime:string}, index) => (
-                                <li key={index}>{getFormattedDateTime(slot.startTime)} to {getFormattedDateTime(slot.endTime)}</li>
-                            ))
-                        }
-                    </ul>
-               </li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>View Doctor Details</h1>
+      <ul>
+        <li>Name: {doctor?.name}</li>
+        <li>Speciality: {doctor?.speciality}</li>
+        <li>Session price: {doctor?.sessionPrice}</li>
+        <li>Email: {doctor?.email}</li>
+        <li>Mobile Number: {doctor?.mobileNumber}</li>
+        <li>Affiliation: {doctor?.affiliation}</li>
+        <li>Educational Background: {doctor?.educationalBackground}</li>
+        <li>
+          Available time slots:
+          <ul>
+            {doctor?.availableSlots &&
+              doctor?.availableSlots.map(
+                (slot: { startTime: string; endTime: string }, index) => (
+                  <li key={index}>
+                    {getFormattedDateTime(slot.startTime)} to{" "}
+                    {getFormattedDateTime(slot.endTime)}
+                  </li>
+                )
+              )}
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default ViewDoctorDetails;
