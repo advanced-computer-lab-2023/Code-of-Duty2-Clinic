b01e78b3a610dd1c9dff941454da310aea36d806
OmarAhmedAdel <omaradel2602@gmail.com>
2023-11-14 19:09:01 +0200

    added final changes

--- server/src/controllers/doctors/addAvailableTimeSlots.ts
+++ server/src/controllers/doctors/addAvailableTimeSlots.ts
@@ -1,46 +1,80 @@
-import { Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { AuthorizedRequest } from "../../types/AuthorizedRequest";
-import { addAvailableSlots } from '../../services/doctors';
-import Appointment from '../../models/appointments/Appointment';
-import Doctor from '../../models/doctors/Doctor';
+import { addAvailableSlots } from "../../services/doctors";
+import Appointment from "../../models/appointments/Appointment";
+import Doctor from "../../models/doctors/Doctor";
 
-export const addDoctorAvailableSlots = async (req: AuthorizedRequest, res: Response) => {
+export const addDoctorAvailableSlots = async (
+  req: AuthorizedRequest,
+  res: Response
+) => {
   try {
     const { startTime, endTime } = req.body;
     const doctorId = req.user?.id!;
 
-    const doctor = await Doctor.findById(doctorId).select('+availableSlots');
+    const doctor = await Doctor.findById(doctorId).select("+availableSlots");
 
     if (!doctor) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Doctor not found' });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Doctor not found" });
+    }
+    if (!startTime || !endTime) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Start time and end time are required" });
     }
 
+    if (startTime >= endTime) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Start time must be less than end time" });
+    }
+
+    if (startTime < new Date()) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Start time must be greater than current time" });
+    }
+
+    if (endTime < new Date()) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "End time must be greater than current time" });
+    }
 
     const existingAppointment = await Appointment.findOne({
       doctorId: doctorId,
-      'timePeriod.startTime': { $lt: endTime },
-      'timePeriod.endTime': { $gt: startTime },
+      "timePeriod.startTime": { $lt: endTime },
+      "timePeriod.endTime": { $gt: startTime },
     });
 
     if (existingAppointment) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'This time slot is occupied with an appointment' });
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "This time slot is occupied with an appointment" });
     }
 
-    const existingAvailableSlot = doctor?.availableSlots.find((slot) => slot.startTime === startTime || slot.endTime === endTime);
+    const existingAvailableSlot = doctor?.availableSlots.find(
+      (slot) => slot.startTime === startTime || slot.endTime === endTime
+    );
 
     if (existingAvailableSlot) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'This time slot is already added' });
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "This time slot is already added" });
     }
 
-    
     await addAvailableSlots(doctorId, startTime, endTime);
 
-    return res.status(StatusCodes.OK).json({ message: 'Available time slots added successfully!' });
-
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Available time slots added successfully!" });
   } catch (error) {
-
     console.error(error);
-    res.status(StatusCodes.BAD_REQUEST).json({ message: 'An error occurred while adding time slots' });
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "An error occurred while adding time slots" });
   }
 };
