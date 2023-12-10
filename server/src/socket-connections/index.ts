5cd04cef8b9a26cf3ff0f2f73146f476f701fc54
AbdelrahmanSalehRewaished <abdelrahman.saleh@student.guc.edu.eg>
2023-12-10 02:55:42 +0200

    added socket.io connections to cancellation and rescheduling handlers

--- server/src/socket-connections/index.ts
+++ server/src/socket-connections/index.ts
@@ -1,30 +1,46 @@
-import { storeNotificationSentToUser as saveNotificationSentToUser } from "../services/notifications";
-import NotificationSubjectDescription from "../types/NotificationSubjectDescription";
 import { User } from "../types/User";
 import SocketType from "../types/SocketType";
+import {
+  rescheduleAppointmentForRegisteredPatientHandler,
+  rescheduleAppointmentForDependentPatientHandler,
+  cancelAppointmentForDependentPatientHandler,
+  cancelAppointmentForRegisteredPatientHandler,
+} from "../controllers/appointments/doctors";
 
 const addSocketEventListeners = (socket: SocketType) => {
   const user = socket.handshake.auth as User;
 
-  socket.on("appointment_rescheduling", (data) => {
-    const notificationMessage: NotificationSubjectDescription = {
-      subject: "Appointment Rescheduled",
-      description: "Notification sent",
-    };
-    saveNotificationSentToUser(user, notificationMessage);
-    socket.to(user.id).emit("notification_message", {
-      message: notificationMessage,
-    });
-  });
+  socket.on("appointment_rescheduling_as_doctor_for_registered", (data) =>
+    rescheduleAppointmentForRegisteredPatientHandler(data, user.id, socket)
+  );
 
-  socket.on("appointment_cancellation", (data) => {
-    const notificationMessage: NotificationSubjectDescription = {
-      subject: "Appointment Rescheduled",
-      description: "Notification sent",
-    };
-    saveNotificationSentToUser(user, notificationMessage);
-    socket.to(user.id).emit("notification2", { message: notificationMessage });
-  });
+  socket.on("appointment_rescheduling_as_doctor_for_dependent", (data) =>
+    rescheduleAppointmentForDependentPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_rescheduling_as_patient_for_registered", (data) =>
+    rescheduleAppointmentForRegisteredPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_rescheduling_as_patient_for_dependent", (data) =>
+    rescheduleAppointmentForDependentPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_cancellation_as_doctor_for_registered", (data) =>
+    cancelAppointmentForRegisteredPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_cancellation_as_doctor_for_dependent", (data) =>
+    cancelAppointmentForDependentPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_cancellation_as_patient_for_registered", (data) =>
+    cancelAppointmentForRegisteredPatientHandler(data, user.id, socket)
+  );
+
+  socket.on("appointment_cancellation_as_patient_for_dependent", (data) =>
+    cancelAppointmentForDependentPatientHandler(data, user.id, socket)
+  );
 };
 
 export default addSocketEventListeners;
