6985c9d18fe79ad41639d268533b076834474d61
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2023-12-14 23:07:54 +0200

    Comment out dark mode switcher

--- client/src/components/trays/Avatar.tsx
+++ client/src/components/trays/Avatar.tsx
@@ -1,18 +1,7 @@
-import {
-  FormControlLabel,
-  IconButton,
-  Menu,
-  MenuItem,
-  Switch,
-} from "@mui/material";
+import { FormControlLabel, IconButton, Menu, MenuItem, Switch } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  AccountCircle,
-  Brightness4,
-  Brightness7,
-  Logout as LogoutIcon,
-} from "@mui/icons-material";
+import { AccountCircle, Brightness4, Brightness7, Logout as LogoutIcon } from "@mui/icons-material";
 
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -20,7 +9,7 @@ import { welcomeRoute } from "../../data/routes/guestRoutes";
 
 const Avatar = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  // const { theme, toggleTheme } = useContext(ThemeContext);
   const { logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -50,20 +39,15 @@ const Avatar = () => {
       >
         <AccountCircle />
       </IconButton>
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem>
+      <Menu id="menu-appbar" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        {/* <MenuItem>
           <FormControlLabel
             control={
               <Switch checked={theme === "dark"} onChange={toggleTheme} />
             }
             label={theme === "dark" ? <Brightness4 /> : <Brightness7 />}
           />
-        </MenuItem>
+        </MenuItem> */}
 
         <MenuItem onClick={handleLogout}>
           <LogoutIcon />
